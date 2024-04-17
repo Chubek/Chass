8dc565e233d0dc5f61dc03add079a64e022e2190
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-04-17 16:37:51 +0330

    Implemented: The getopt interface

--- src/chass.c
+++ src/chass.c
@@ -7,27 +7,37 @@
 
 #include "chass.h"
 
-static struct option long_options[] = {{"file", required_argument, 0, 'f'},
+static struct option long_options[] = {{"cfgfile", optional_argument, 0, 'f'},
                                        {"key", required_argument, 0, 'k'},
-                                       {"obscure", no_argument, 0, 'o'},
+                                       {"clipboard", no_argument, 0, 'c'},
+                                       {"help", no_argument, 0, 'h'},
                                        {0, 0, 0, 0}};
 
 int main(int argc, char *argv[]) {
   int opt;
   int option_index = 0;
 
+  char *config_file = CONFIG_FILE_DEFAULT;
+  uint8_t *key = NULL;
+  bool clipboard = false;
+
   while ((opt = getopt_long(argc, argv, "f:k:o", long_options,
                             &option_index)) != -1) {
     switch (opt) {
     case 'f':
+      config_file = optarg;
       break;
     case 'k':
+      key = optarg;
       break;
-    case 'o':
+    case 'c':
+      clipboard = true;
       break;
-    case '?':
+    case 'h':
+      display_help_and_exit();
+      break;
     default:
-      exit(EXIT_FAILURE);
+      break;
     }
   }
 
