3917b89103c782f339b77140dbf04ccf259b8987
Chubak Bidpaa <chubakbidpaa@gmail.com>
2024-04-18 00:57:21 +0330

    Snapshot: #2

--- src/chass.c
+++ src/chass.c
@@ -7,39 +7,38 @@
 
 #include "chass.h"
 
-static struct option long_options[] = {{"cfgfile", optional_argument, 0, 'f'},
-                                       {"key", required_argument, 0, 'k'},
-                                       {"clipboard", no_argument, 0, 'c'},
-                                       {"help", no_argument, 0, 'h'},
-                                       {0, 0, 0, 0}};
+
+static struct option 
+long_options[] = {
+	 {"cfg", optional_argument, 0, 'f'},
+         {"key", required_argument, 0, 'k'},
+	 {"req", required_argument, 0, 'r'},
+         {"help", no_argument, 0, 'h'},
+         {0, 0, 0, 0}
+};
+
+
 
 int main(int argc, char *argv[]) {
-  int opt;
-  int option_index = 0;
-
-  char *config_file = CONFIG_FILE_DEFAULT;
-  uint8_t *key = NULL;
-  bool clipboard = false;
-
-  while ((opt = getopt_long(argc, argv, "f:k:o", long_options,
+  while ((opt = getopt_long(argc, argv, "f:k:r:c", long_options,
                             &option_index)) != -1) {
     switch (opt) {
     case 'f':
-      config_file = optarg;
+      cfg = optarg;
       break;
     case 'k':
       key = optarg;
       break;
-    case 'c':
-      clipboard = true;
+    case 'r':
+      req = optarg;
       break;
     case 'h':
-      display_help_and_exit();
-      break;
+      display_help();
+      return EXIT_FAILURE;
     default:
+      warning_out("Warning: wrong arg %c", opt);
       break;
     }
   }
 
-  return 0;
 }
